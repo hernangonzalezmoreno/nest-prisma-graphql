59d4e21108c8185be52636c57f1875df09beac0d
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-07-16 17:46:24 -0300

    Resource Users + scheme.gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
